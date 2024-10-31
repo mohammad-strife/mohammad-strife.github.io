5ae96666bb8dc49e2fcc1c5488baad67e0985c68
mohammad <mohamad.zaarei@gmail.com>
2024-10-31 23:30:19 +0330

    fix: selected article

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<HomePage />} loader={useArticleLoader} />
         {/* Private Routes */}
         <Route path="" element={<PrivateRoute />}>
           <Route path="/profile" element={<ProfilePage />} />
